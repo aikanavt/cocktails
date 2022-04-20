72b68ba31c716df07796b532255f5d0a541adc7c
aikanavt <sagadiyevaaa@gmail.com>
2022-04-20 21:34:38 +0600

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut.
+        Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan
+        marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear
+        claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi
+        bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie
+        gummies. Jelly beans tiramisu pudding. Toffee soufflé chocolate cake
+        pastry brownie. Oat cake halvah sweet roll cotton candy croissant
+        lollipop. Macaroon tiramisu chocolate bar candy candy carrot cake jelly
+        sweet. Gummies croissant macaroon dessert. Chocolate cake dragée pie.
+      </p>
+    </section>
+  );
+};
+
+export default About;
